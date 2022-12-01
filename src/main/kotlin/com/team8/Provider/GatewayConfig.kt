45c1d266b3d1982054aaea244c00a0b79c138ab8
richardpine189 <73888797+richardpine189@users.noreply.github.com>
2022-12-01 11:50:36 -0300

    change Gateway config

--- src/main/kotlin/com/team8/Provider/GatewayConfig.kt
+++ src/main/kotlin/com/team8/Provider/GatewayConfig.kt
@@ -1,7 +1,7 @@
 package com.team8.Provider
 
 object GatewayConfig {
-    val userAPI = "https://calm-sands-92296.herokuapp.com"
-    //val userAPI = "http://0.0.0.0:8083"
+    //val userAPI = "https://calm-sands-92296.herokuapp.com"
+    val userAPI = "http://0.0.0.0:8083"
     val localRepositoryPath = ""
 }
